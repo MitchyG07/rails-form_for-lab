7c5dafddfc3a31e6e5378ffa02704827b6999ae0
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-12-15 13:24:43 -0800

    tests pass

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20201215173956) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "schoolclasses", force: :cascade do |t|
+    t.string  "title"
+    t.integer "room_number"
+  end
+
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
